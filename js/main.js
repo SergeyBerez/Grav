539caefdad740003e5460aa6652fd1ad31d65bff
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2020-01-14 20:26:14 +0200

    add files git

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('.header_sliders').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    infinite: true,
+  });
+});
